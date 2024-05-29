ca63f89987b1093764a4cadda73efdae54e42072
ZENSE <125728307+ZENSE-TH@users.noreply.github.com>
2024-05-29 17:10:23 +0700

    create pages with react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import React from 'react';
+
+// Import Pages
+import { Home } from './pages/Home.jsx'
+import { About } from './pages/About.jsx'
+import { Projects } from './pages/Projects.jsx'
+import { Signin } from './pages/Signin.jsx'
+import { Signup } from './pages/Signup.jsx'
+import { Dashboard } from './pages/Dashboard.jsx'
+
 
 export default function App() {
   return (
-    <h1 className='text-3xl font-bold underline text-red-700'>Main</h1>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/projects' element={< Projects/>}></Route>
+          <Route path='/dashboard' element={<Dashboard/>}></Route>
+          <Route path='/signin' element={<Signin/>}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <h1 className='text-red-500 text-bold'></h1>
+    </>
   )
 }
