9f36dc24ddf38c72778a3a20ed77b141eb0d19bb
ZENSE <125728307+ZENSE-TH@users.noreply.github.com>
2024-06-02 15:51:43 +0700

    Create and Connect mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(
+        ()  => { console.log('Mongodb is running!') }
+    )
+
 const app = express();
 
 app.listen(3000,() => {
